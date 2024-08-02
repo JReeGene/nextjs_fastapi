6b4d134f415e4b84e1d89a52b1dbda54d6e5fb72
Jeremiah <jgitaga@gmail.com>
2024-08-02 18:35:25 +0300

    Connected to PostgreSQL and SQLite

--- api/core/config.py
+++ api/core/config.py
@@ -1,5 +1,18 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 class Settings:
     PROJECT_TITLE: str = 'Fullstack NextJS/FastAPI Site'
     PROJECT_VERSION: str = '0.0.1'
+
+    POSTGRES_USER: str = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_SERVER:str = os.getenv('POSTGRES_SERVER', 'localhost')
+    POSTGRES_PORT:str = os.getenv('POSTGRES_PORT', 5432) #DEFAULT
+    POSTGRES_DB:str = os.getenv('POSTGRES_DB', 'nextjs_fastapi_db')
+    DATABASE_URL: str = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 settings = Settings()
--- api/index.py
+++ api/index.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from .core.config import settings
+from .database.session import engine
+from .database.base_class import Base 
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables() 
+    return app
+
+app = start_application()
 
 @app.get("/")
 def hello_world():
